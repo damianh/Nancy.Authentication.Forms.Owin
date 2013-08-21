989357b0d891db2594eddcbf3b7a16048cf30d5b
ThomasWeiss <thomas@additivelabs.com>
2013-08-21 15:22:57 +0800

    Update NancyAuthMiddleware.cs
    Tab formatting...

--- src/App/NancyAuthMiddleware.cs
+++ src/App/NancyAuthMiddleware.cs
@@ -34,13 +34,13 @@
             }
             NancyCookie authCookie = GetFormsAuthCookies(requestHeaders["Cookie"]).SingleOrDefault();
             if (authCookie == null)
-			{
-				return _next.Invoke(environment);
-			}
+            {
+                return _next.Invoke(environment);
+            }
             string user = FormsAuthentication.DecryptAndValidateAuthenticationCookie(authCookie.Value, _formsAuthenticationConfiguration);
             Guid userId;
-			if (Guid.TryParse(user, out userId))
-			{
+            if (Guid.TryParse(user, out userId))
+            {
                 ClaimsPrincipal claimsPrincipal = _userManager.GetClaimsPrincial(Guid.Parse(userId));
                 if (environment.ContainsKey(ServerUser))
                 {
@@ -50,21 +50,21 @@
                 {
                     environment.Add(ServerUser, claimsPrincipal);
                 }
-			}
+            }
             return _next.Invoke(environment);
         }
 
-		private IEnumerable<NancyCookie> GetFormsAuthCookies(string[] cookieHeaders)
-		{
-			foreach (var header in cookieHeaders.Select(h => h.Split(';')))
-			{
-				yield return header.Select(c =>
-				{
-					var pair = c.Split('=');
-					return new NancyCookie(pair[0], pair[1]);
-				})
-				.SingleOrDefault(c => c.Name == FormsAuthentication.FormsAuthenticationCookieName);
-			}
-		}
-	}
+        private IEnumerable<NancyCookie> GetFormsAuthCookies(string[] cookieHeaders)
+        {
+            foreach (var header in cookieHeaders.Select(h => h.Split(';')))
+            {
+                yield return header.Select(c =>
+                {
+                    var pair = c.Split('=');
+                    return new NancyCookie(pair[0], pair[1]);
+                })
+                .SingleOrDefault(c => c.Name == FormsAuthentication.FormsAuthenticationCookieName);
+            }
+        }
+    }
 }
