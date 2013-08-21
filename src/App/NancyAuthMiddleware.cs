70e722cf8097c72046c9418f5747335975d796e2
ThomasWeiss <thomas@additivelabs.com>
2013-08-21 15:20:37 +0800

    Update NancyAuthMiddleware.cs
    Made it work when request has multiple session cookies & when FormsAuth decryption returns unexpected values.

--- src/App/NancyAuthMiddleware.cs
+++ src/App/NancyAuthMiddleware.cs
@@ -32,24 +32,39 @@
             {
                 return _next.Invoke(environment);
             }
-            NancyCookie authCookie = requestHeaders["Cookie"]
-                .Select(c =>
-                        {
-                            var pair = c.Split('=');
-                            return new NancyCookie(pair[0], pair[1]);
-                        })
-                .SingleOrDefault(c => c.Name == FormsAuthentication.FormsAuthenticationCookieName);
-            string userId = FormsAuthentication.DecryptAndValidateAuthenticationCookie(authCookie.Value, _formsAuthenticationConfiguration);
-            ClaimsPrincipal claimsPrincipal = _userManager.GetClaimsPrincial(Guid.Parse(userId));
-            if (environment.ContainsKey(ServerUser))
-            {
-                environment[ServerUser] = claimsPrincipal;
-            }
-            else
-            {
-                environment.Add(ServerUser, claimsPrincipal);
-            }
+            NancyCookie authCookie = GetFormsAuthCookies(requestHeaders["Cookie"]).SingleOrDefault();
+            if (authCookie == null)
+			{
+				return _next.Invoke(environment);
+			}
+            string user = FormsAuthentication.DecryptAndValidateAuthenticationCookie(authCookie.Value, _formsAuthenticationConfiguration);
+            Guid userId;
+			if (Guid.TryParse(user, out userId))
+			{
+                ClaimsPrincipal claimsPrincipal = _userManager.GetClaimsPrincial(Guid.Parse(userId));
+                if (environment.ContainsKey(ServerUser))
+                {
+                    environment[ServerUser] = claimsPrincipal;
+                }
+                else
+                {
+                    environment.Add(ServerUser, claimsPrincipal);
+                }
+			}
             return _next.Invoke(environment);
         }
-    }
+
+		private IEnumerable<NancyCookie> GetFormsAuthCookies(string[] cookieHeaders)
+		{
+			foreach (var header in cookieHeaders.Select(h => h.Split(';')))
+			{
+				yield return header.Select(c =>
+				{
+					var pair = c.Split('=');
+					return new NancyCookie(pair[0], pair[1]);
+				})
+				.SingleOrDefault(c => c.Name == FormsAuthentication.FormsAuthenticationCookieName);
+			}
+		}
+	}
 }
